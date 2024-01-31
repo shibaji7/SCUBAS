8a315dbcc705c0db865498502f701efc9688d22e
Shibaji Chakraborty <shibaji7@vt.edu>
2024-01-31 14:20:25 -0500

    EVA & QU

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="scubas",
-    version="0.1.4",
+    version="0.1.5",
     packages=["scubas"],
     package_dir={"scubas": "scubas"},
     package_data={"scubas": []},
